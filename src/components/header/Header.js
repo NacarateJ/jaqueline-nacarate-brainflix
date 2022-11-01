843cb153ded2073096347704dacc085af042c27e
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-10-31 20:26:30 -0700

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import "./header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      Hello
+      {/* ALL THE HTML GOES HERE */}
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/mainContent/MainContent.js
@@ -0,0 +1,11 @@
+import "./mainContent.scss";
+
+function MainContent() {
+    return (
+        <main>HTML goes here</main>
+    )
+
+
+}
+
+export default MainContent;

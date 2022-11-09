4486d9d750feaa3639d0565dc9bb9c0c7cc3f53a
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-11-08 23:59:57 -0800

    commented out event listener, getting data through useEffect/axios

--- src/components/heroVideo/HeroVideo.js
+++ src/components/heroVideo/HeroVideo.js
@@ -24,45 +24,6 @@ const HeroVideo = ({ video }) => {
           controls
         ></video>
       </section>
-
-      {/* <section className="hero__information">
-        <h1 className="hero__information-caption">{title}</h1>
-
-        <div className="hero__information-wrapper">
-          <div className="hero__information-left">
-            <p className="hero__information-channel"> By {channel}</p>
-            <p className="hero__information-timestamp">
-              {formatDistance(new Date(timestamp), new Date(), {
-                addSuffix: true,
-              })}
-            </p>
-          </div>
-
-          <div className="hero__information-right">
-            <div className="hero__information-container">
-              <img
-                className="hero__information-icon"
-                src={Views}
-                alt="gray eye icon"
-              ></img>
-              <p className="hero__information-numbers">{views}</p>
-            </div>
-
-            <div className="hero__information-container">
-              <img
-                className="hero__information-icon"
-                src={Likes}
-                alt="gray heart icon"
-              ></img>
-              <p className="hero__information-numbers">{likes}</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="hero__information-description">
-          <p>{description}</p>
-        </div>
-      </section> */}
     </main>
   );
 };

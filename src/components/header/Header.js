737615c36d4c592726f0ee96c2fe36434c6b5a7a
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-11-12 19:51:48 -0800

    fixing css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,42 +1,51 @@
 import "./header.scss";
 import React from "react";
 import Logo from "../../assets/logo/BrainFlix.svg";
-import Image from "../../assets/images/Mohan-muruge.jpg";
+// import Image from "../../assets/images/Mohan-muruge.jpg";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
-      {/* <div className="header__navigation-left"> */}
       <Link to={"/"} className="header__company-logo-wrapper">
-        <img className="header__company-logo"
+        <img
+          className="header__company-logo"
           src={Logo}
           alt="Play button on a blue backgorund and BrainFlix word to the right"
         ></img>
       </Link>
-      {/* </div> */}
 
-      <nav className="header__navigation-right">
+      <div className="header__navigation-right">
         <div className="header__navigation-right-wrapper">
           <input
             className="header__search"
             type="search"
             placeholder="Search"
           ></input>
-          <div className="header__avatar-container">
-            <img
-              className="header__avatar"
-              src={Image}
-              alt="Mohan Muruge profile facing left"
-            ></img>
-          </div>
+          <div className="header__avatar-container"></div>
         </div>
         <Link to="/uploads">
           <button className="header__button" type="text">
             UPLOAD
           </button>
         </Link>
-      </nav>
+      </div>
+
+      <div className="header__navigation-right-medium">
+        {/* <div className="header__navigation-right-wrapper"> */}
+          <input
+            className="header__search"
+            type="search"
+            placeholder="Search"
+          ></input>
+          <div className="header__avatar-container"></div>
+        <Link to="/uploads">
+          <button className="header__button" type="text">
+            UPLOAD
+          </button>
+        </Link>
+        {/* </div> */}
+      </div>
     </header>
   );
 };

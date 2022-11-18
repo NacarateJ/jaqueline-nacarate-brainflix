9be721bc90ece2774dbda86982b1baef3a71d708
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-11-17 23:53:59 -0800

    upload pic from browser done

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,7 +7,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 // const mainVideo = `https://project-2-api.herokuapp.com/videos/84e96018-4022-434e-80bf-000ce4cd12b8?api_key=${process.env.REACT_APP_API_KEY}`;
-const sideBarVideos = `https://project-2-api.herokuapp.com/videos/?api_key=${process.env.REACT_APP_API_KEY}`;
+// const sideBarVideos = `https://project-2-api.herokuapp.com/videos/?api_key=${process.env.REACT_APP_API_KEY}`;
+
+const BACK_END = process.env.REACT_APP_BACKEND_URL;
 
 const HomePage = () => {
   // useState & useEffect for sidedar
@@ -18,26 +20,24 @@ const HomePage = () => {
 
     const fetchData = async () => {
       try {
-        const { data } = await axios.get(sideBarVideos);
-        if (videoId) {
-          const videos = data.filter(
-            (video) =>
-              video.id !== videoId
-          );
+        const { data } = await axios.get(`${BACK_END}/api/videos`);
+        // if (videoId) {
+          const videos = data.filter((video) => video.id !== videoId);
           setVideos(videos);
-        } else {
-          const videos = data.filter(
-            (video) =>
-              video.id !== "84e96018-4022-434e-80bf-000ce4cd12b8"
-          );
-          setVideos(videos);
-        }
+        // } 
+        // else {
+        //   const videos = data.filter(
+        //     (video) => video.id !== 
+        //   );
+        //   setVideos(videos);
+        // }
       } catch (error) {
         console.log("Error", error);
       }
     };
     fetchData();
   }, [params.videoId]);
+
 
   // useState & useEffect for Hero Video and Comments
   const [videoDetails, setVideoDetails] = useState({});
@@ -49,13 +49,13 @@ const HomePage = () => {
       setVideoId(params.videoId);
       urlVideoId(videoId);
     } else {
-      const defaultVideoId = "84e96018-4022-434e-80bf-000ce4cd12b8";
+      const defaultVideoId = `${BACK_END}/api/videos/${videoId}`;
       setVideoId(defaultVideoId);
       urlVideoId(defaultVideoId);
     }
 
     function urlVideoId(videoId) {
-      const mainVideo = `https://project-2-api.herokuapp.com/videos/${videoId}?api_key=${process.env.REACT_APP_API_KEY}`;
+      const mainVideo = `${BACK_END}/api/videos/${videoId}`;
       const fetchData = async () => {
         try {
           const { data } = await axios.get(mainVideo);
--- src/pages/UploadsPage/UploadsPage.js
+++ src/pages/UploadsPage/UploadsPage.js
@@ -2,8 +2,31 @@ import "./uploadsPage.scss";
 import "../../components/header/Header";
 import Image from "../../assets/images/Upload-video-preview.jpg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+
+
+const BACK_END = process.env.REACT_APP_BACKEND_URL;
 
 const UploadsPage = () => {
+
+  const [videos, setVideos] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newVideo = {
+      title: event.target.videoTitle.value,
+      description: event.target.videoDescripition.value,
+    };
+    axios.post(`${BACK_END}/api/videos`, newVideo).then((response) => {
+      setVideos([...videos, response.data]);
+    });
+    event.target.reset();
+  };
+
+ 
+
   return (
     <>
       <section className="upload">
@@ -12,24 +35,25 @@ const UploadsPage = () => {
           <div className="video">
             <p className="video__title">VIDEO THUMBNAIL</p>
             <div className="video__poster-wrapper">
-              <img
+              {/* <img
                 className="video__poster"
                 src={Image}
                 alt="View of a blue bike and it's speedometer from the top by the rider"
-              ></img>
+              ></img> */}
             </div>
+          
           </div>
 
-          <form autoComplete="off" className="form">
+          <form onSubmit={handleSubmit} autoComplete="off" className="form">
             <div className="form__video">
               <label className="form__video-label" htmlFor="text">
                 TITLE YOUR VIDEO
               </label>
               <textarea
                 className="form__video-input form__video-input--active"
-                id="text"
+                id="videoTitle"
                 type="text"
-                name="text"
+                name="videoTitle"
                 rows="1"
                 cols="30"
                 placeholder="Add tittle to your video"
@@ -42,31 +66,37 @@ const UploadsPage = () => {
               </label>
               <textarea
                 className="form__description-input form__description-input--active"
-                id="text"
+                id="videoDescripition"
                 type="text"
-                name="text"
+                name="videoDescripition"
                 rows="5"
                 cols="30"
                 placeholder="Add description to your video"
               ></textarea>
             </div>
+
+            <div className="button">
+              <div className="button__wrapper-publish">
+                {/* <Link to="/upload-successful"> */}
+                <button
+                  // onSubmit={handleSubmit}
+                  className="button__publish"
+                  type="submit"
+                >
+                  PUBLISH
+                </button>
+                {/* </Link> */}
+              </div>
+
+              <div className="button__wrapper">
+                <Link to="/">
+                  <button className="button__cancel" type="button">
+                    CANCEL
+                  </button>
+                </Link>
+              </div>
+            </div>
           </form>
-        </div>
-
-        <div className="button">
-          <div className="button__wrapper-publish">
-            <Link to="/upload-successful">
-              <button className="button__publish" type="file">
-                PUBLISH
-              </button>
-            </Link>
-          </div>
-
-          <div className="button__wrapper">
-            <button className="button__cancel" type="button">
-              CANCEL
-            </button>
-          </div>
         </div>
       </section>
     </>
